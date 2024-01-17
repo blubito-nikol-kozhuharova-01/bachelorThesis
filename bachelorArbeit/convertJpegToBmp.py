c2dc40fdf95a16055510603ee600ff418e607e81
blubito-nikol-kozhuharova-01 <nikol.dcqu.kozhuharova@misumi.co.jp>
2024-01-17 15:18:07 +0200

    refactor image folder structure and add region growing to gui

--- bachelorArbeit/convertJpegToBmp.py
+++ bachelorArbeit/convertJpegToBmp.py
@@ -1,10 +1,10 @@
 from PIL import Image
 
 # Open the JPEG image
-jpeg_image = Image.open('./images/1-01-Copy.jpg')
+jpeg_image = Image.open('./images/adrenal-3C-edit.jpg')
 
 # Convert the image to BMP format
 bmp_image = jpeg_image.convert('RGB')
 
 # Save the BMP image
-bmp_image.save('./images/1-01-Copy.bmp')
+bmp_image.save('./images/adrenal-3C-edit.bmp')
